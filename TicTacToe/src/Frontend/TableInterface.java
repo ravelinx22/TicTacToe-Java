376e7de2858b9b8b2ea46d48a44c2447d66dbb72
William Ravelo <drummerwilliam@gmail.com>
2016-04-17 22:25:17 -0500

    Created and added the buttons that will represent each estate, to the principal window.

--- TicTacToe/src/Frontend/TableInterface.java
+++ TicTacToe/src/Frontend/TableInterface.java
@@ -6,6 +6,15 @@ import java.awt.*;
 
 public class TableInterface extends JFrame 
 {
+	private JButton square1;
+	private JButton square2;
+	private JButton square3;
+	private JButton square4;
+	private JButton square5;
+	private JButton square6;
+	private JButton square7;
+	private JButton square8;
+	private JButton square9;
 
 	public TableInterface()
 	{
@@ -18,6 +27,28 @@ public class TableInterface extends JFrame
 		setBackground(Color.BLACK);
 		setLayout(new GridLayout(3,3));
 		this.getContentPane().setBackground(Color.WHITE);
+		
+		//Initialize the button
+		square1 = new JButton();
+		square2 = new JButton();
+		square3 = new JButton();
+		square4 = new JButton();
+		square5 = new JButton();
+		square6 = new JButton();
+		square7 = new JButton();
+		square8 = new JButton();
+		square9 = new JButton();
+		
+		//Add to the layout the components
+		add(square1);
+		add(square2);
+		add(square3);
+		add(square4);
+		add(square5);
+		add(square6);
+		add(square7);
+		add(square8);
+		add(square9);
 	}
 
 
