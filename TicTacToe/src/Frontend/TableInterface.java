576ec9fb0bb26fd2be669600a6b82df4e3c510ea
William Ravelo <drummerwilliam@gmail.com>
2016-04-17 22:06:16 -0500

    Created the game window

--- TicTacToe/src/Frontend/TableInterface.java
+++ TicTacToe/src/Frontend/TableInterface.java
@@ -1,10 +1,27 @@
 package Frontend;
 import Backend.*;
+import javax.swing.*;
+import java.awt.*;
 
-public class TableInterface {
 
+public class TableInterface extends JFrame 
+{
+	
+	public TableInterface()
+	{
+		setSize(400,400);
+		setTitle("Tic Tac Toe");
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(new GridLayout(3,3));
+		setLocationRelativeTo(null);
+		
+	}
+
+	
 	public static void main(String args[])
 	{
-
+       TableInterface window = new TableInterface();
+       window.setVisible(true);
 	}
 }
