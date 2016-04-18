d06455b0f3c0758491e4dc2171a35c8888bbe2e8
William Ravelo <drummerwilliam@gmail.com>
2016-04-17 22:17:27 -0500

    Added Constants

--- TicTacToe/src/Backend/Table.java
+++ TicTacToe/src/Backend/Table.java
@@ -2,4 +2,19 @@ package Backend;
 
 public class Table {
 
+	/**
+	 * Represents the starting point of the square.
+	 */
+	public final static int START = 0;
+	
+	/**
+	 * Represents a cross in the square.
+	 */
+	public final static int CROSS = 1;
+	
+	/**
+	 * Represents a circle in the square.
+	 */
+	public final static int CIRCLE = 2;
+	
 }
