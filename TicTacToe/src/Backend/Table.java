0f1df2017b7d565c93fd726821267cb49e8e3ba8
William Ravelo <drummerwilliam@gmail.com>
2016-04-17 22:33:32 -0500

    Wrote the constructor of Table and created the resetTable() method

--- TicTacToe/src/Backend/Table.java
+++ TicTacToe/src/Backend/Table.java
@@ -6,15 +6,54 @@ public class Table {
 	 * Represents the starting point of the square.
 	 */
 	public final static int START = 0;
-	
+
 	/**
 	 * Represents a cross in the square.
 	 */
 	public final static int CROSS = 1;
-	
+
 	/**
 	 * Represents a circle in the square.
 	 */
 	public final static int CIRCLE = 2;
-	
+
+	/**
+	 * Player has won?
+	 */
+	private boolean won;
+
+
+	/**
+	 * Squares
+	 */
+	private int squares[][];
+
+	/**
+	 * Constructor.
+	 */
+	public Table()
+	{
+		squares =  new int[][]{
+			{START,START,START},
+			{START,START,START},
+			{START,START,START}
+		};
+
+		won = false;
+	}
+
+	/**
+	 * Resets the table to the initial state.
+	 */
+	private void resetTable()
+	{
+		for(int i = 0; i < squares.length; i++)
+		{
+			for(int j = 0; j < squares[i].length; j++)
+			{
+				squares[i][j] = START;
+			}
+		}
+	}
+
 }
